05f331e596d70d0fcae91eb9ad9ae7bb2ca334da
eagaratez <45834790+eagaratez@users.noreply.github.com>
2019-10-03 11:19:25 -0300

    Renaming and other changes
    Renaming and other changes

--- ExportToXML/ExportToXMLv2.js
+++ ExportToXML/ExportToXMLv2.js
@@ -64,9 +64,7 @@ function ( qlik, $, cssContent, props,filesaver) {
 				var dataObject ={}
 				var j=0;
 				for (var i=0;i<colcount;i++){
-					if(dimensionInfo[i].qFallbackTitle){
-						
-						
+					if(dimensionInfo[i].qFallbackTitle){						
 						dataObject[dimensionInfo[i].qFallbackTitle] = dataItem[i].qText;
 					}
 					else {
@@ -77,7 +75,76 @@ function ( qlik, $, cssContent, props,filesaver) {
 				//console.log("dataObject",dataObject);
 				return dataObject;
 			}
-			
+
+            me.getAllresults = function (dimensions, measures) {
+                var deferred = $q.defer(),
+                    promises = [],
+                    qTotalData = [],
+                    qTotalData2 = [],
+                    qDimensions = [],
+                    qMeasures = [];
+                    
+                if (dimensions.length) {
+                    angular.forEach(dimensions, function(value, key) {
+                        qDimensions.push({ 
+                            qDef: { 
+                                qGrouping: "N", 
+                                qFieldDefs: [ value ], 
+                                qSortCriterias: [{
+                                    qSortByAscii: (value==='SeriesName') ? 1 : 0, 
+                                }],
+                            } 
+                        });
+                    });
+                }
+                if (measures.length) {
+                    angular.forEach(measures, function(value, key) {
+                        qMeasures.push({ 
+                            qDef : { 
+                                qDef : value
+                            }
+                        });
+                    });
+                }
+                app.obj.app.createCube({
+                    qDimensions : qDimensions,
+                    qMeasures : qMeasures
+                }).then(function(model) {
+                    model.getHyperCubeData('/qHyperCubeDef', [{qTop: 0, qWidth: 20, qLeft: 0, qHeight: 500}]).then(function(data) {
+                        var columns = model.layout.qHyperCube.qSize.qcx;
+                        var totalheight = model.layout.qHyperCube.qSize.qcy;
+                        var pageheight = Math.floor(10000 / columns);
+                        var numberOfPages = Math.ceil(totalheight / pageheight);
+                        if (numberOfPages==1) {
+                            deferred.resolve(data.qDataPages[0].qMatrix);
+                        } else {
+                            utility.log('Download Started on', new Date());
+                            var Promise = $q;
+                            var promises = Array.apply(null, Array(numberOfPages)).map(function(data, index) {
+                                var page = {
+                                    qTop: (pageheight * index) + index,
+                                    qLeft: 0,
+                                    qWidth: columns,
+                                    qHeight: pageheight,
+                                    index: index
+                                };						
+                                return model.getHyperCubeData('/qHyperCubeDef', [page]);
+                            }, this);					
+                            Promise.all(promises).then(function(data) {
+                                for (var j=0; j<data.length; j++) {
+                                    for (var k=0; k<data[j].qDataPages[0].qMatrix.length; k++) {							
+                                        qTotalData.push(data[j].qDataPages[0].qMatrix[k])
+                                    }
+                                }
+                                utility.log('Download Ended on', new Date());
+                                deferred.resolve(qTotalData);
+                            });
+                        }
+                    })
+                })
+                return deferred.promise;
+            }
+
 			function getMoreData(colcount,rowcount,rowsTofetch,totalRows,callback){
 						if(rowsTofetch>=totalRows){
 						callback(rowcount,jsonData);
@@ -112,9 +179,7 @@ function ( qlik, $, cssContent, props,filesaver) {
 						
 						}
 			}
-					
-			
-			
+						
 			var rowcount,totalRows;
 			$('#'+ObjId+'>button').on('click',function(){
 					qlik.currApp().getObjectProperties(chartId).then(function(model) {
@@ -124,24 +189,9 @@ function ( qlik, $, cssContent, props,filesaver) {
 					 totalRows = hypercube.qSize.qcy;
 					var rowsTofetch = Math.floor(10000/colcount);
 					rowcount = rowsTofetch; //maximum allowed columns are 20 --> thats why initial qheight is set to 10000/20 = 500
-					
-					
-  				    model.getHyperCubeData('/qHyperCubeDef', [{
-
-
-					qTop: 0, 
-
-
-					qLeft: 0, 
-
-
-					qWidth: colcount, 
-
-
-					qHeight: rowsTofetch
-
-
-				  }]).then(function(data){
+										
+					  model.getHyperCubeData('/qHyperCubeDef', [{	qTop: 0,qLeft: 0,qWidth: colcount,qHeight: rowsTofetch}])
+					  .then(function(data){
 						//var dataPage = data[0].qMatrix;
 						dataToExport.push(data[0].qMatrix);
 						var data = data[0].qMatrix;
@@ -163,8 +213,11 @@ function ( qlik, $, cssContent, props,filesaver) {
 									
 							jsonData[jsonTableName]= dataArray;
 							//console.log("dataArray",dataArray);
+
+                            var content = JSON.stringify(jsonData);
+                            console.log('Entro al JSON');
 							var content = json2xml(JSON.stringify(jsonData),tab);
-							
+
 							// any kind of extension (.txt,.cpp,.cs,.bat)
                             //var filename = jsonTableName+".json";
                             var filename = jsonTableName+".xml";
